fa6c954f218712330d8c185e0fbf793ade8f429f
Antoine Brun <antoine.brunSK8@live.fr>
2021-11-29 13:49:00 +0100

    ex02 todo

--- Module_3/ex01/Fixed.cpp
+++ Module_3/ex01/Fixed.cpp
@@ -6,19 +6,21 @@
 /*   By: abrun <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 19:47:54 by abrun             #+#    #+#             */
-/*   Updated: 2021/11/28 20:53:43 by abrun            ###   ########.fr       */
+/*   Updated: 2021/11/29 13:38:33 by abrun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+#include <string>
+#include <cmath>
+#include <iomanip>
+	
+const int	Fixed::_bits = 8;
 
 Fixed::Fixed(void)
 {
 	std::cout << "Default constructor called" << std::endl;
-	int const	raw = 0;
-	std::string	str;
-
-	setRawBits(raw);
+	this->_raw = 0;
 }
 
 Fixed::Fixed(const Fixed &fixeed)
@@ -32,47 +34,49 @@ Fixed::~Fixed(void)
 	std::cout << "Destructor called" << std::endl;
 }
 
-Fixed & Fixed::operator=(const Fixed &fixeed)
+Fixed& Fixed::operator=(const Fixed &fixeed)
 {
 	std::cout << "Assignation member called" << std::endl;
-	setRawBits(fixeed.getRawBits());
+	this->_raw = fixeed.getRawBits();
 	return (*this);
 }
 
 Fixed::Fixed(const int n)
 {
 	std::cout << "Int constructor called" << std::endl;
-	setRawBits(n);
+	setRawBits(n << _bits);
 }
 
+//	1 << _bits : on décale à gauche de _bits la representation par bits de 1
 Fixed::Fixed(const float n)
 {
+
 	std::cout << "Float constructor called" << std::endl;
-	setRawBits(n);
+	this->_raw = roundf(n * (1 << _bits));
 }
 
 int	Fixed::getRawBits(void) const
 {
-	return (this->fix);
+	return (this->_raw);
 }
 
 void	Fixed::setRawBits(int const raw)
 {
-	this->fix = raw;
+	this->_raw = raw;
 }
 
 float	Fixed::toFloat(void) const
 {
-	float	res;
-
-	res = getRawBits();
-	return (res);
+	return ((float)this->_raw / (1 << _bits));
 }
 
-float	Fixed::toInt(void) const
+int		Fixed::toInt(void) const
 {
-	int	res;
+	return (getRawBits() >> _bits);
+}
 
-	res = roundf(getRawBits());
-	return (res);
+std::ostream& operator<<(std::ostream& os, const Fixed& nbr)
+{
+	os << nbr.toFloat();
+	return (os);
 }
--- Module_3/ex01/Fixed.hpp
+++ Module_3/ex01/Fixed.hpp
@@ -6,32 +6,32 @@
 /*   By: abrun <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 19:38:15 by abrun             #+#    #+#             */
-/*   Updated: 2021/11/28 20:45:10 by abrun            ###   ########.fr       */
+/*   Updated: 2021/11/29 13:38:13 by abrun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
 # include <iostream>
-# include <string>
-# include <cmath>
 
 class	Fixed
 {
-	protected:
-		int			fix;
-		static const int	bits = 8;
+	private:
+		int			_raw;
+		static const int	_bits;
 	
 	public:
 		Fixed();
 		Fixed(const Fixed&);
 		~Fixed();
-		Fixed &operator=(const Fixed&);
+		Fixed& operator=(const Fixed&);
 		Fixed(const int n);
 		Fixed(const float n);
 		int	getRawBits(void) const;
 		void	setRawBits(int const raw);
 		float	toFloat(void) const;
-		float	toInt(void) const;
+		int		toInt(void) const;
 };
+
+std::ostream& operator<<(std::ostream& os, const Fixed& nbr);
 #endif

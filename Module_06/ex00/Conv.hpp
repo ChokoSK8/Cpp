14d2a3041d34280977fe185bf1b5ea065abc539e
Antoine Brun <antoine.brunSK8@live.fr>
2022-02-03 12:25:26 +0100

    conversion scalaire

--- Module_06/ex00/Conv.hpp
+++ Module_06/ex00/Conv.hpp
@@ -6,7 +6,7 @@
 /*   By: abrun <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 10:21:27 by abrun             #+#    #+#             */
-/*   Updated: 2022/02/01 10:21:35 by abrun            ###   ########.fr       */
+/*   Updated: 2022/02/03 12:14:36 by abrun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,27 @@
 class	Conv
 {
 	private:
-			float	_float;
-			int		_int;
-			char	_char;
-			double	_double;
+			std::string	_float;
+			std::string	_int;
+			std::string	_char;
+			std::string	_double;
+			int		_type;
 
 	public:
 			Conv(std::string);
 			~Conv(void);
-			float	toFloat(std::string);
-			int		toInt(std::string);
-			char	toChar(std::string);
-			double	toDouble(std::string);
-			void	displayConv(const Conv);
+			std::string	getFloat(void) const;
+			std::string	getInt(void) const;
+			std::string	getChar(void) const;
+			std::string	getDouble(void) const;
+			int		getTypeof(std::string);
+			void	fillTypes(std::string);
+			void	toFloat(std::string);
+			void	toInt(std::string);
+			void	toChar(std::string);
+			void	toDouble(std::string);
+			void	displayConv(void);
 };
+
+std::ostream& operator<<(std::ostream& os, const Conv& convi);
 #endif
--- Module_06/ex00/main.cpp
+++ Module_06/ex00/main.cpp
@@ -6,12 +6,13 @@
 /*   By: abrun <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 09:40:47 by abrun             #+#    #+#             */
-/*   Updated: 2022/02/01 09:57:49 by abrun            ###   ########.fr       */
+/*   Updated: 2022/02/03 11:49:25 by abrun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
+#include "Conv.hpp"
 
 int	main(int ac, char **av)
 {
@@ -19,5 +20,7 @@ int	main(int ac, char **av)
 		return (1);
 	std::string str(av[1]);
 	std::cout << "argument : " + str << std::endl;
+	Conv	arg(str);
+	std::cout << arg << std::endl;
 	return (0);
 }
